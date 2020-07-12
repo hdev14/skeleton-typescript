372a61545809b972cc6d15c4edf124edd6973f2d
HermersonDev <ne.hermerson@gmail.com>
2020-07-12 10:46:36 -0300

    :wrench: add env, typeorm and database config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 
+import './database'
+
 import routes from './routes'
+require('dotenv').config()
 
 const app = express()
 app.use(express.json())
@@ -9,6 +12,7 @@ app.use(cors())
 
 app.use(routes)
 
-app.listen(4444, () => {
-  console.log('Server is running!')
+const port = process.env.APP_PORT || 3333
+app.listen(port, () => {
+  console.log(`Server is running!\n http://localhost:${port}`)
 })
